6ed7d802d809de7880f4e6d621cda3d3a5d82902
MC <62519464+Wyzbidul@users.noreply.github.com>
2022-06-17 12:50:02 +0900

    Update streamline.py

--- streamline.py
+++ streamline.py
@@ -17,6 +17,7 @@ print('START TESTS')
 def f(x,y):
     return (-y,x)
 
+seed = array([[1,0]])
 x1 = 1
 y1 = 0
 X,Y = [],[]
@@ -36,26 +37,24 @@ for k in range(4):
 
 #Plot Results
 figure()
-quiver(X,Y,U,V,angles='xy', scale_units='xy', scale=1)
+quiver(X,Y,U,V,angles='xy', scale_units='xy', scale=1, color='C0')
+plot([1],[0], 'ro')
 xticks([1,0,-1,0],X)
 yticks([0,1,0,-1],Y)
-xlim(-1,1)
-ylim(-1,1)
 grid()
 show()
 
-w=2
+w=2.1
 Y2,X2 = mgrid[-w:w+1,-w:w+1]
 U2,V2 = f(X2,Y2)[:][0],f(X2,Y2)[:][1]
-seed = array([[1,0]])
 
 
 figure()
 streamplot(X2,Y2,U2,V2,start_points=seed)
+quiver([1],[0],[0],[1],angles='xy', scale_units='xy', scale=8, color='C0')
+plot([1],[0], 'ro')
 xticks([1,0,-1,0],X)
 yticks([0,1,0,-1],Y)
-xlim(-1,1)
-ylim(-1,1)
 grid()
 show()
 
